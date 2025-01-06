fb1343986781d6f6f04f4bf09d8ffee8599efda3
Tarrylon Toney <Tarrylon@gmail.com>
2025-01-05 18:25:25 -0600

    learning methods

--- IfKeywordAndCodeBlocks/src/MainChallenge.java
+++ IfKeywordAndCodeBlocks/src/MainChallenge.java
@@ -7,11 +7,7 @@ public class MainChallenge {
         int bonus = 100;
 
         int finalScore = score;
-
-        if (gameOver) {
-            finalScore += (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
+        calculateScore(true, 800, 5, 100);
 
         score = 10000;
         levelCompleted = 8;
@@ -24,5 +20,16 @@ public class MainChallenge {
         }
 
 
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        int finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
+        }
     }
 }
